1df751619b7a400e41c7fd516feb43a07b62f3ad
ctyar <ctyar@outlook.com>
2025-07-26 10:08:31 +0100

    Remove serilog

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,8 +1,5 @@
 using Aiui;
 using Microsoft.Extensions.AI;
-using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.SystemConsole.Themes;
 using Web.Components;
 
 namespace Web;
@@ -11,86 +8,57 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        try
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.AddServiceDefaults();
+
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        builder.AddAzureOpenAIClient("openai")
+            .AddChatClient("gpt-4o-mini")
+            .UseFunctionInvocation()
+            .UseOpenTelemetry(configure: c =>
+                c.EnableSensitiveData = builder.Environment.IsDevelopment());
+
+        builder.Services.AddSingleton(new SqlListPlugin(builder.Configuration.GetConnectionString("SqlServer")!,
+        [
+            "Products",
+            "Categories",
+            "CustomerCustomerDemo",
+            "CustomerDemographics",
+            "Customers",
+            "Employees",
+            "EmployeeTerritories",
+            "Order Details",
+            "Orders",
+            "Region",
+            "Shippers",
+            "Suppliers",
+            "Territories"
+        ]));
+
+        builder.Services.AddSingleton<ChartJsPlugin>();
+
+        var app = builder.Build();
+
+        app.MapDefaultEndpoints();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            AddLogging(builder);
-
-            builder.AddServiceDefaults();
-
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
-
-            builder.AddAzureOpenAIClient("openai")
-                .AddChatClient("gpt-4o-mini")
-                .UseFunctionInvocation()
-                .UseOpenTelemetry(configure: c =>
-                    c.EnableSensitiveData = builder.Environment.IsDevelopment());
-
-            builder.Services.AddSingleton(new SqlListPlugin(builder.Configuration.GetConnectionString("SqlServer")!,
-            [
-                "Products",
-                "Categories",
-                "CustomerCustomerDemo",
-                "CustomerDemographics",
-                "Customers",
-                "Employees",
-                "EmployeeTerritories",
-                "Order Details",
-                "Orders",
-                "Region",
-                "Shippers",
-                "Suppliers",
-                "Territories"
-            ]));
-
-            builder.Services.AddSingleton<ChartJsPlugin>();
-
-            var app = builder.Build();
-
-            app.MapDefaultEndpoints();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error", createScopeForErrors: true);
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseAntiforgery();
-
-            app.UseStaticFiles();
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
-
-            app.Run();
+            app.UseExceptionHandler("/Error", createScopeForErrors: true);
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
-        catch (Exception ex)
-        {
-            Log.Fatal(ex, "Application terminated unexpectedly");
-        }
-        finally
-        {
-            Log.CloseAndFlush();
-        }
-    }
 
-    private static void AddLogging(WebApplicationBuilder builder)
-    {
-        var loggerConfiguration = new LoggerConfiguration();
-        loggerConfiguration.Enrich.FromLogContext();
-        loggerConfiguration.Enrich.WithProperty("Version", Version.Current);
+        app.UseHttpsRedirection();
+        app.UseAntiforgery();
 
-        loggerConfiguration.MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning);
-        loggerConfiguration.MinimumLevel.Override("Web", LogEventLevel.Debug);
+        app.UseStaticFiles();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
 
-        loggerConfiguration.WriteTo.Console(theme: AnsiConsoleTheme.Code);
-
-        Log.Logger = loggerConfiguration.CreateLogger();
-
-        builder.Services.AddSerilog();
+        app.Run();
     }
 }
